ff509317f28ceabc1d49b3cdfb6b1746e1827ef8
Benjamin Temple <ben.temple@gmail.com>
2020-12-07 23:37:35 -0800

    Solved problem 8

--- src/main/kotlin/day_08/Problem_08_1.kt
+++ src/main/kotlin/day_08/Problem_08_1.kt
@@ -5,9 +5,50 @@ import sourcePath
 import java.io.*
 
 class Problem_08_1() : Solution {
-    override fun solve(): String {
-        File("${sourcePath}/day_08/input.txt").forEachLine { input ->
+    private enum class Instruction(val value: String) {
+        Acc("acc"),
+        Jmp("jmp"),
+        Nop("nop");
+
+        companion object {
+            fun from(value: String): Instruction {
+                return values().first { it.value == value }
+            }
         }
-        return "Problem 08-1:\n Solution"
+    }
+
+    override fun solve(): String {
+        val instructions: MutableList<Pair<Instruction, Int>> = mutableListOf()
+        File("${sourcePath}/day_08/input.txt").forEachLine { input ->
+            input.split(" ").let {
+                instructions.add(Pair(Instruction.from(it[0]), it[1].toInt()))
+            }
+        }
+        var accumulator: Int = 0
+        var nextInstruction: Int = 0
+        val runInstructions = mutableListOf<Int>()
+        do {
+            if (runInstructions.contains(nextInstruction)) {
+//                println("Started repeating instructions. Quitting.")
+                break
+            }
+            runInstructions.add(nextInstruction)
+            instructions[nextInstruction].let {
+                when (it.first) {
+                    Instruction.Acc -> {
+                        accumulator += it.second
+                        nextInstruction++
+                    }
+                    Instruction.Jmp -> {
+                        nextInstruction += it.second
+                    }
+                    Instruction.Nop -> {
+                        nextInstruction++
+                    }
+                }
+            }
+        } while (nextInstruction != 0)
+
+        return "${this::class.java.simpleName}:\n Accumulator after first loop: $accumulator"
     }
 }
--- src/main/kotlin/day_08/Problem_08_2.kt
+++ src/main/kotlin/day_08/Problem_08_2.kt
@@ -5,9 +5,83 @@ import sourcePath
 import java.io.*
 
 class Problem_08_2() : Solution {
-    override fun solve(): String {
-        File("${sourcePath}/day_08/input.txt").forEachLine { input ->
+    private enum class Instruction(val value: String) {
+        Acc("acc"),
+        Jmp("jmp"),
+        Nop("nop");
+
+        companion object {
+            fun from(value: String): Instruction {
+                return values().first { it.value == value }
+            }
         }
-        return "Problem 08-2:\n Solution"
+    }
+
+    override fun solve(): String {
+        val instructions: MutableList<Pair<Instruction, Int>> = mutableListOf()
+        File("${sourcePath}/day_08/input.txt").forEachLine { input ->
+            input.split(" ").let {
+                instructions.add(Pair(Instruction.from(it[0]), it[1].toInt()))
+            }
+        }
+
+        var nextInstructionToTestFrom = 0
+        var accumulatorValue: Int? = null
+        do {
+            val modifiedInstructions = instructions.toMutableList()
+            nextInstructionToTestFrom = modifiedInstructions.modifyNextInstruction(nextInstructionToTestFrom)
+            accumulatorValue = calculateAccumulatorValue(modifiedInstructions)
+        } while (accumulatorValue == null)
+
+        return "${this::class.java.simpleName}:\n Accumulator after fixing the loop: $accumulatorValue"
+    }
+
+    private fun MutableList<Pair<Instruction, Int>>.modifyNextInstruction(nextInstructionToTestFrom: Int): Int {
+        for (j in nextInstructionToTestFrom..size) {
+            get(j).let {
+                when (it.first) {
+                    Instruction.Jmp -> {
+                        set(j, Pair(Instruction.Nop, it.second))
+                        return j+1
+                    }
+                    Instruction.Nop -> {
+                        set(j, Pair(Instruction.Jmp, it.second))
+                        return j+1
+                    }
+                    Instruction.Acc -> {
+                        // Do nothing
+                    }
+                }
+            }
+        }
+        return Integer.MAX_VALUE
+    }
+
+    private fun calculateAccumulatorValue(instructions: List<Pair<Instruction, Int>>): Int? {
+        var accumulator: Int = 0
+        var nextInstruction: Int = 0
+        val runInstructions = mutableListOf<Int>()
+        do {
+            if (runInstructions.contains(nextInstruction)) {
+//                println("Started repeating instructions. Quitting.")
+                return null
+            }
+            runInstructions.add(nextInstruction)
+            instructions[nextInstruction].let {
+                when (it.first) {
+                    Instruction.Acc -> {
+                        accumulator += it.second
+                        nextInstruction++
+                    }
+                    Instruction.Jmp -> {
+                        nextInstruction += it.second
+                    }
+                    Instruction.Nop -> {
+                        nextInstruction++
+                    }
+                }
+            }
+        } while (nextInstruction < instructions.size)
+        return accumulator
     }
 }
